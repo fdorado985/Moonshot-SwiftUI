04ba9c082edc18e6e7a30bcb54c7a7fe3897f798
Juan Dorado <fdorado985@gmail.com>
2020-11-28 00:51:08 -0600

    Initial Commit

---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Juan Francisco Dorado Torres on 28/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
