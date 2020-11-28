b4b330b5860af7b0d75c0449640e254c44be7c80
Juan Dorado <fdorado985@gmail.com>
2020-11-28 09:43:01 -0600

    Wrap the Text on a custom struct to show how the performance is affected on scroll views

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -12,12 +12,25 @@ struct ContentView: View {
     ScrollView(.vertical) {
       VStack(spacing: 10) {
         ForEach(0..<100) {
-          Text("Item \($0)")
+          CustomText("Item \($0)")
             .font(.title)
         }
       }
       .frame(maxWidth: .infinity)
     }
+  }
+}
+
+struct CustomText: View {
+  var text: String
+
+  var body: some View {
+    Text(text)
+  }
+
+  init(_ text: String) {
+    print("Creating a new CustomText")
+    self.text = text
   }
 }
 
